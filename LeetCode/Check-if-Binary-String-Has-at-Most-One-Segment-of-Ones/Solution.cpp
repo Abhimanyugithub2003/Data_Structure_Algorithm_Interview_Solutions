504c003b552830b6a72284868408e56967e39b21
Abhimanyu Singh Parihar <149948798+Abhimanyugithub2003@users.noreply.github.com>
2026-03-06 23:07:20 +0530

    Refactor checkOnesSegment to use stack for logic

--- LeetCode/Check-if-Binary-String-Has-at-Most-One-Segment-of-Ones/Solution.cpp
+++ LeetCode/Check-if-Binary-String-Has-at-Most-One-Segment-of-Ones/Solution.cpp
@@ -1,6 +1,19 @@
 class Solution {
 public:
     bool checkOnesSegment(string s) {
-        return !s.contains("01") ;
+        int n=s.length();
+        stack<char> st;
+        int i=1;
+        st.push('1');
+        while(i<n){
+            char ch=s[i];
+        if(!st.empty()){
+            if(st.top()=='0' && ch=='1')
+            return false;
+            st.push(ch);
+        }
+        i++;
+        }
+        return true;
     }
 };
