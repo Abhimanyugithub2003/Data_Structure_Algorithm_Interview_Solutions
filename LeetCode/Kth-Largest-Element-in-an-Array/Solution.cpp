d5b3d2883face09fdde7ff30c2e544f2d6730bb2
Abhimanyu Singh Parihar <149948798+Abhimanyugithub2003@users.noreply.github.com>
2026-05-03 11:10:25 +0530

    Add solution for Kth Largest Element in an Array

--- LeetCode/Kth-Largest-Element-in-an-Array/Solution.cpp
+++ LeetCode/Kth-Largest-Element-in-an-Array/Solution.cpp
@@ -1,16 +1,13 @@
 1class Solution {
 2public:
 3    int findKthLargest(vector<int>& nums, int k) {
-4        priority_queue<int,vector<int>, greater<int>>q;
-5        for(int i=0;i<k;i++)
+4        priority_queue<int>q;
+5        for(int i=0;i<nums.size();i++)
 6        q.push(nums[i]);
-7        while(k<nums.size()){
-8            if(q.top()<nums[k]){
-9                q.pop();
-10                q.push(nums[k]);
-11            }
-12            k++;
-13        }
-14        return q.top();
-15    }
-16};
+7        k--;
+8        while(k--){
+9            q.pop();
+10        }
+11        return q.top();
+12    }
+13};
