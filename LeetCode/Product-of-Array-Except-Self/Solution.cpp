847fc205711d45a3ff3cf34f4e589f4409c99192
Abhimanyu Singh Parihar <149948798+Abhimanyugithub2003@users.noreply.github.com>
2026-05-03 16:15:32 +0530

    Add solution for Product of Array Except Self

--- LeetCode/Product-of-Array-Except-Self/Solution.cpp
+++ LeetCode/Product-of-Array-Except-Self/Solution.cpp
@@ -2,27 +2,20 @@
 2public:
 3    vector<int> productExceptSelf(vector<int>& nums) {
 4        int n=nums.size();
-5        int count0=0;
-6        int totalproduct=1;
-7        int idx=0;
-8        for(int i=0;i<n;i++){
-9            if(nums[i]==0){
-10                count0++;
-11                idx=i;
-12                continue;
-13            }
-14            totalproduct*=nums[i];
-15        }
-16        vector<int> ans(n,0);
-17        if(count0==1){
-18            ans[idx]=totalproduct;
-19            return ans;
-20        }else if(count0>1){
-21            return ans;
-22        }
-23        for(int i=0;i<n;i++){
-24            ans[i]=totalproduct/nums[i];
-25        }
-26        return ans;
-27    }
-28};
+5        vector<int> prefix(n);
+6        prefix[0]=1;
+7        for(int i=1;i<n;i++){
+8            prefix[i]=prefix[i-1]*nums[i-1];
+9        }
+10        vector<int> sufix(n);
+11        sufix[n-1]=1;
+12        for(int i=n-2;i>=0;i--){
+13            sufix[i]=sufix[i+1]*nums[i+1];
+14        }
+15        vector<int> ans(n);
+16        for(int i=0;i<n;i++){
+17            ans[i]=prefix[i]*sufix[i];
+18        }
+19        return ans;
+20    }
+21};
