918ae28478c0dd6328148ae51c7ba50767db6cee
Abhimanyu Singh Parihar <149948798+Abhimanyugithub2003@users.noreply.github.com>
2026-05-09 13:55:11 +0530

    Add solution for Two Sum

--- LeetCode/Two-Sum/Solution.cpp
+++ LeetCode/Two-Sum/Solution.cpp
@@ -1,13 +1,16 @@
 1class Solution {
 2public:
 3    vector<int> twoSum(vector<int>& nums, int target) {
-4        for(int i = 0; i < nums.size(); i++) {
-5            for(int j = i + 1; j < nums.size(); j++) {
-6                if(nums[i] + nums[j] == target) {
-7                    return {i, j};
-8                }
-9            }
-10        }
-11        return {};
-12    }
-13};
+4        int n=nums.size();
+5        unordered_map<int,int> mp;
+6        for(int i=0;i<n;i++){
+7            int first=nums[i];
+8            int second=target-first;
+9            if(mp.find(second)!=mp.end()){
+10                return {i,mp[second]};
+11            }
+12            mp[first]=i;
+13        }
+14        return {-1,-1};
+15    }
+16};
