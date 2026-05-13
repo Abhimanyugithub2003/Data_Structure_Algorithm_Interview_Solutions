10478bd510827ed9b6e1367cf15260d3792fbd66
Abhimanyu Singh Parihar <149948798+Abhimanyugithub2003@users.noreply.github.com>
2026-05-13 08:51:17 +0530

    Add solution for Valid Anagram

--- LeetCode/Valid-Anagram/Solution.cpp
+++ LeetCode/Valid-Anagram/Solution.cpp
@@ -1,27 +1,17 @@
 1class Solution {
 2public:
 3    bool isAnagram(string s, string t) {
-4        int n=s.length(),m=t.length();
-5        if(n!=m) return false;
-6        unordered_map<char,int> mp1;
-7        unordered_map<char,int> mp2;
-8        for(auto &it: s){
-9            mp1[it]++;
-10        }
-11        for(auto &it: t){
-12            mp2[it]++;
-13        }
-14        int i=0;
-15        while(i<n){
-16            if(mp1.find(s[i])!=mp1.end() ){
-17                if(mp2.find(s[i])==mp2.end())
-18                 return false;
-19                else{
-20                    if(mp1[s[i]]!=mp2[s[i]]) return false;
-21                }
-22            }
-23            i++;
-24        }
-25        return true;
-26    }
-27};
+4        if(s.length()!=t.length()) return false;
+5        int freq[26]={0};
+6        for(char &it: s){
+7            freq[it-'a']++;
+8        }
+9        for(char&it : t){
+10            freq[it-'a']--;
+11        }
+12        for(int i=0;i<26;i++){
+13            if(freq[i]>0) return false;
+14        }
+15        return true;
+16    }
+17};
