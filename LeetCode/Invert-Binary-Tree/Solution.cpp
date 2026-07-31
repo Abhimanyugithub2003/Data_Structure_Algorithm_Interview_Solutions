90d88d562b55597b5c86d4ca1c3babc875210aee
Abhimanyu Singh Parihar <149948798+Abhimanyugithub2003@users.noreply.github.com>
2026-07-31 12:50:34 +0530

    Add solution for Invert Binary Tree

--- LeetCode/Invert-Binary-Tree/Solution.cpp
+++ LeetCode/Invert-Binary-Tree/Solution.cpp
@@ -13,23 +13,11 @@
 13public:
 14    TreeNode* invertTree(TreeNode* root) {
 15        if(!root) return nullptr;
-16        queue<TreeNode*> q;
-17        q.push(root);
-18        while(!q.empty()){
-19            int N = q.size();
-20            while(N--){
-21                auto node = q.front();
-22                q.pop();
-23                swap(node->left, node->right);
-24                if(node->left){   
-25                    q.push(node->left);
-26                }
-27                if(node->right){ 
-28                    q.push(node->right);
-29                }   
-30
-31            }
-32        }
-33        return root;
-34    }
-35};
+16        TreeNode* temp=root->left;
+17        root->left=root->right;
+18        root->right=temp;
+19        invertTree(root->left);
+20        invertTree(root->right);
+21        return root;
+22    }
+23};
