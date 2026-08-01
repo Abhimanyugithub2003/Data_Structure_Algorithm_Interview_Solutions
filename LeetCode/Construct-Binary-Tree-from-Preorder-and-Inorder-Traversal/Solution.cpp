133ff288987f434fd407c1410984ef15f7e6e961
Abhimanyu Singh Parihar <149948798+Abhimanyugithub2003@users.noreply.github.com>
2026-08-01 10:30:35 +0530

    Add solution for Construct Binary Tree from Preorder and Inorder Traversal

--- LeetCode/Construct-Binary-Tree-from-Preorder-and-Inorder-Traversal/Solution.cpp
+++ LeetCode/Construct-Binary-Tree-from-Preorder-and-Inorder-Traversal/Solution.cpp
@@ -11,21 +11,25 @@
 11 */
 12class Solution {
 13public:
-14    int find(vector<int> in,int target,int start,int end){
-15        for(int i=start;i<=end;i++){
-16            if(in[i]==target) return i;
-17        }
-18        return -1;
-19    }
-20    TreeNode* tree(vector<int>& in,vector<int> &pre,int instart,int inend,int index){
-21        if(instart>inend) return nullptr;
-22        TreeNode* root=new TreeNode(pre[index]);
-23        int pos=find(in,pre[index],instart,inend);
-24        root->left=tree(in,pre,instart,pos-1,index+1);
-25        root->right=tree(in,pre,pos+1,inend,index+(pos-instart)+1);
-26        return root;
-27    }
-28    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
-29        return tree(inorder,preorder,0,preorder.size()-1,0);    
+14    int serach(vector<int>& inorder, int left, int right, int val){
+15        for(int i=left;i<=right;i++){
+16            if(inorder[i]==val){
+17                return i;
+18            }
+19        }
+20        return -1;
+21    }
+22    TreeNode* buildTreeFromPreAndIn(vector<int>& preorder, vector<int>& inorder,int &preIdx,int left, int right){
+23        if(left>right) return nullptr;
+24        TreeNode* root = new TreeNode(preorder[preIdx]);
+25        int inIdx = serach(inorder, left, right, preorder[preIdx]);
+26        preIdx++;
+27        root->left = buildTreeFromPreAndIn(preorder, inorder, preIdx,left, inIdx - 1);
+28        root->right = buildTreeFromPreAndIn(preorder, inorder, preIdx, inIdx + 1, right);
+29        return root;
 30    }
-31};
+31    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+32        int preIdx = 0;
+33        return buildTreeFromPreAndIn(preorder, inorder, preIdx, 0, inorder.size() - 1);
+34    }
+35};
