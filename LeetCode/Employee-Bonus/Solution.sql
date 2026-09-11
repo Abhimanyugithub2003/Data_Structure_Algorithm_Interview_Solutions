81d134982595062e012cd9ee87cf61a841f2d22b
Abhimanyu Singh Parihar <149948798+Abhimanyugithub2003@users.noreply.github.com>
2026-09-12 02:16:25 +0530

    Add solution for Employee Bonus

--- LeetCode/Employee-Bonus/Solution.sql
+++ LeetCode/Employee-Bonus/Solution.sql
@@ -1,15 +1,6 @@
-import pandas as pd
-
-def employee_bonus(employee: pd.DataFrame, bonus: pd.DataFrame) -> pd.DataFrame:
-    # Merge Employee and Bonus tables using a left join
-    result_df = pd.merge(employee, bonus, on='empId', how='left')
-
-    # Filter rows where bonus is less than 1000 or missing
-    result_df = result_df[(result_df['bonus'] < 1000) | result_df['bonus'].isnull()]
-
-    # Select "name" and "bonus" columns
-    result_df = result_df[['name', 'bonus']]
-
-    return result_df
-
-
+1# Write your MySQL query statement below
+2SELECT e.name, b.bonus FROM Employee AS e
+3LEFT JOIN Bonus AS b
+4ON e.empId = b.empId
+5WHERE b.bonus < 1000
+6OR b.bonus IS NULL;
