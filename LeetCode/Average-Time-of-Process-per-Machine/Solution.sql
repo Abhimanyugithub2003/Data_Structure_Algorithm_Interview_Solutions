d4320248605ba6bc051d278ba52efab8a31ebb57
Abhimanyu Singh Parihar <149948798+Abhimanyugithub2003@users.noreply.github.com>
2026-09-12 02:11:51 +0530

    Add solution for Average Time of Process per Machine

--- LeetCode/Average-Time-of-Process-per-Machine/Solution.sql
+++ LeetCode/Average-Time-of-Process-per-Machine/Solution.sql
@@ -1,1 +1,9 @@
-activity['timestamp'] = activity.apply(lambda x: x.timestamp * -1 if x.activity_type == 'start' else x.timestamp, axis=1)
+1# Write your MySQL query statement below
+2SELECT a1.machine_id, ROUND(AVG(a2.timestamp - a1.timestamp), 3) AS processing_time
+3FROM Activity AS a1
+4JOIN Activity AS a2
+5ON a1.machine_id = a2.machine_id
+6AND a1.process_id = a2.process_id
+7WHERE a1.activity_type = 'start' 
+8AND a2.activity_type = 'end'
+9GROUP BY a1.machine_id;
