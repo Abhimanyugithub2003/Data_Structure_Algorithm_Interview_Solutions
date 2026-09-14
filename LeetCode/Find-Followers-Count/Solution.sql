fe70f78fc0cf5abadb6b15111ee9bbdc79d2564b
Abhimanyu Singh Parihar <149948798+Abhimanyugithub2003@users.noreply.github.com>
2026-09-15 00:31:34 +0530

    Add solution for Find Followers Count

--- LeetCode/Find-Followers-Count/Solution.sql
+++ LeetCode/Find-Followers-Count/Solution.sql
@@ -1,3 +1,2 @@
 1# Write your MySQL query statement below
-2SELECT user_id, COUNT(follower_id) AS followers_count FROM Followers GROUP BY user_id 
-3ORDER BY user_id ASC;
+2SELECT user_id, COUNT(follower_id) AS followers_count FROM Followers GROUP BY user_id ORDER BY user_id ASC;
