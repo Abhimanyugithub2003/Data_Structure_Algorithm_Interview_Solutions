4557962d58454143d5f8c45702cede7962dcfae9
Abhimanyu Singh Parihar <149948798+Abhimanyugithub2003@users.noreply.github.com>
2026-09-15 02:00:11 +0530

    Add solution for The Number of Employees Which Report to Each Employee

--- LeetCode/The-Number-of-Employees-Which-Report-to-Each-Employee/Solution.sql
+++ LeetCode/The-Number-of-Employees-Which-Report-to-Each-Employee/Solution.sql
@@ -1,6 +1,7 @@
 1# Write your MySQL query statement below
-2SELECT e1.employee_id, e1.name, COUNT(e2.employee_id) AS reports_count, ROUND(AVG(e2.age)) AS average_age FROM Employees AS e1
-3JOIN Employees AS e2
-4ON e1.employee_id = e2.reports_to
-5GROUP BY e1.employee_id, e1.name 
-6ORDER BY e1.employee_id;
+2SELECT e1.employee_id AS employee_id, e1.name, COUNT(e2.employee_id) AS reports_count, ROUND(AVG(e2.age), 0) AS average_age FROM Employees AS e1
+3JOIN
+4Employees AS e2
+5ON e1.employee_id = e2.reports_to
+6GROUP BY e1.employee_id, e1.name
+7ORDER BY e1.employee_id;
